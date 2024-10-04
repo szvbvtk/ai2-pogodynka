05472aae3c3a2ea5972196f50efe49a4a9ed4608
Krystian Szabat <krystian.szabat@outlook.com>
2024-10-04 18:59:09 +0200

    .

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -6,13 +6,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+use App\Entity\Location;
+use App\Repository\MeasurementEntryRepository;
+
 class WeatherController extends AbstractController
 {
-    #[Route('/weather/{id}', name: 'app_weather', requirements: ['id' => '\d+'])]
-    public function city(): Response
+    #[Route('/weather/{country}/{city}', name: 'app_weather', requirements: ['id' => '\d+'])]
+    public function city(Location $location, MeasurementEntryRepository $measurementEntryRepository): Response
     {
-        return $this->render('weather/index.html.twig', [
-            'controller_name' => 'WeatherController',
+        $measurementEntries = $measurementEntryRepository->findByLocation($location);
+
+        return $this->render('weather/city.html.twig', [
+            'location' => $location,
+            'measurement_entries' => $measurementEntries,
         ]);
+
     }
 }
--- src/Repository/MeasurementEntryRepository.php
+++ src/Repository/MeasurementEntryRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Entity\Location;
 use App\Entity\MeasurementEntry;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -14,6 +15,20 @@ class MeasurementEntryRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, MeasurementEntry::class);
+    }
+
+    public function findByLocation(Location $location) 
+    {
+        $gb = $this->createQueryBuilder('m');
+        $gb->where('m.location = :location')
+            ->setParameter('location', $location)
+            ->andWhere('m.dateTime > :now')
+            ->setParameter('now', new \DateTime('now'));
+
+            $query = $gb->getQuery();
+            $result = $query->getResult();
+
+            return $result;
     }
 
     //    /**
