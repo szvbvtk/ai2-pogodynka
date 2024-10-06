bfb0816d29215ae596599b7c38da80c727f63b59
Krystian Szabat <krystian.szabat@outlook.com>
2024-10-06 15:09:33 +0200

    chore: Update LocationType form to include city, latitude, longitude, and country fields

--- src/Form/LocationType.php
+++ src/Form/LocationType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Location;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +14,35 @@ class LocationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('city')
-            ->add('country')
-            ->add('latitude')
-            ->add('longitude')
+            ->add('city', null, [
+                'attr' => ['placeholder' => 'Enter city name', 'autofocus' => true],
+            ])
+            ->add('latitude', NumberType::class, [
+                // doc - If set to true, the HTML input will be rendered as a native HTML5 type="number" form. 
+                'html5' => true,
+                'scale' => 6,
+            ])
+            ->add('longitude', NumberType::class, [
+                'html5' => true,
+                'scale' => 7,
+            ])
+            ->add('country', ChoiceType::class, [
+                'choices' => [
+                    'Poland' => 'PL',
+                    'Italy' => 'IT',
+                    'Germany' => 'DE',
+                    'France' => 'FR',
+                    'Canada' => 'CA',
+                    'Spain' => 'ES',
+                    'China' => 'CN',
+                    'Russia' => 'RU',
+                    'United States' => 'US',
+                    'Japan' => 'JP',
+                    'United Kingdom' => 'GB',
+                    'Australia' => 'AU',
+                    'Brazil' => 'BR',
+                ],
+            ]);
         ;
     }
 
