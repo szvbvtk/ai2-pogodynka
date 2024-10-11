7b332649ab57028511a6c7e5b5716200a552fbb0
Krystian Szabat <krystian.szabat@outlook.com>
2024-10-11 18:56:03 +0200

    .

--- src/Controller/WeatherApiController.php
+++ src/Controller/WeatherApiController.php
@@ -2,18 +2,46 @@
 
 namespace App\Controller;
 
+use App\Entity\MeasurementEntry;
+use App\Service\WeatherUtil;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
 
 class WeatherApiController extends AbstractController
 {
-    #[Route('/weather/api', name: 'app_weather_api')]
-    public function index(): JsonResponse
-    {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/WeatherApiController.php',
-        ]);
-    }
+    #[Route('/api/v1/weather', name: 'app_weather_api')]
+    public function index(
+        #[MapQueryParameter(filter: \FILTER_VALIDATE_REGEXP, options: ['regexp' => '/^[a-zA-Z-]+$/'])] string $city,
+        #[MapQueryParameter(filter: \FILTER_VALIDATE_REGEXP, options: ['regexp' => '/^[a-zA-Z]{2}$/'])] string $country,
+        WeatherUtil $weatherUtil,
+    ): JsonResponse {
+
+        $city = str_replace('-', ' ', $city);
+        $city = ucfirst($city);
+
+        $measurementEntries = $weatherUtil->getWeatherForCountryAndCity($country, $city);
+
+        return $this->json(
+            [
+                'city' => $city,
+                'country' => $country,
+                'measurements' => array_map(
+                    fn(MeasurementEntry $m) => [
+                        'temperature_celcius' => $m->getTemperatureCelcius(),
+                        'feels_like' => $m->getFeelsLike(),
+                        'humidity' => $m->getHumidity(),
+                        'pressure' => $m->getPressure(),
+                        'timestamp' => $m->getDateTime()->format('Y-m-d H:i:s'),
+                        'wind_speed' => $m->getWindSpeed(),
+                        'wind_direction' => $m->getWindDirection(),
+                    ],
+                    $measurementEntries
+                ),
+            ]
+            );
+
+}
+
 }
