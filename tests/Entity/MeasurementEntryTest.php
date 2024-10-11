b19573c6b872cb42ebfd35c4eb043c08d60e4e9d
Krystian Szabat <krystian.szabat@outlook.com>
2024-10-11 20:45:38 +0200

    lab G

--- tests/Entity/MeasurementEntryTest.php
+++ tests/Entity/MeasurementEntryTest.php
@@ -7,20 +7,30 @@ use PHPUnit\Framework\TestCase;
 
 class MeasurementEntryTest extends TestCase
 {
-    public function testGetFahrenheit(): void
+    public function dataGetFahrenheit(): array
+    {
+        return [
+            ['0', 32],
+            ['-100', -148],
+            ['100', 212],
+            ['37', 98.6],
+            ['100.4', 212.72],
+            ['98.6', 209.48],
+            ['5', 41],
+            ['0.1', 32.18],
+            ['-0.1', 31.82],
+            ['-5', 23],
+        ];
+    }
+
+    /**
+     * @dataProvider dataGetFahrenheit
+     */
+    public function testGetFahrenheit($celsius, $expectedFahrenheit): void
     {
         $measurement = new MeasurementEntry();
-        
-        // Test 1: celcius = 0, fahrenheit = 32
-        $measurement->setTemperatureCelcius(0);
-        $this->assertEquals(32, $measurement->getFahrenheit());
 
-        // Test 2: celcius = -100, fahrenheit = -148
-        $measurement->setTemperatureCelcius(-100);
-        $this->assertEquals(-148, $measurement->getFahrenheit());
-
-        // Test 3: celcius = 100, fahrenheit = 212
-        $measurement->setTemperatureCelcius(100);
-        $this->assertEquals(212, $measurement->getFahrenheit());
+        $measurement->setTemperatureCelcius($celsius);
+        $this->assertEquals($expectedFahrenheit, $measurement->getFahrenheit());
     }
 }
